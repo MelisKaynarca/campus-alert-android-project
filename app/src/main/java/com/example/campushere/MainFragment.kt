57d3a8961cbc3584108b760684859d4318b27c35
MelisKaynarca <kaynarcamelis@gmail.com>
2025-12-09 20:28:10 +0300

    Some nav graph properties add, login register design make

--- app/src/main/java/com/example/campushere/MainFragment.kt
+++ app/src/main/java/com/example/campushere/MainFragment.kt
@@ -6,7 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class RegisterFragment : Fragment() {
+
+class MainFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +19,7 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
 
--- app/src/main/java/com/example/campushere/PasswordresetFragment.kt
+++ app/src/main/java/com/example/campushere/PasswordresetFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class RegisterFragment : Fragment() {
+
+
+class PasswordresetFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +20,7 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        return inflater.inflate(R.layout.fragment_passwordreset, container, false)
     }
 
 
