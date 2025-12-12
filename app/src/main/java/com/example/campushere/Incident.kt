d1dcc50fc0a1ef8ef8a923122c3da00e141b2dda
MelisKaynarca <kaynarcamelis@gmail.com>
2025-12-12 22:44:32 +0300

    Fragment for create new incident, create new data class for moduler structure

---
+++ app/src/main/java/com/example/campushere/Incident.kt
@@ -0,0 +1,7 @@
+package com.example.campushere
+
+import com.google.type.Date
+
+data class Incident(val title : String,val description: String, val date : Long=System.currentTimeMillis(),val status : String="open"){
+
+}
