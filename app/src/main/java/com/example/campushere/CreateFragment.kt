b7d3dee0034d15536f43d34c4a49fb7bb1355f6f
MelisKaynarca <kaynarcamelis@gmail.com>
2025-12-13 22:32:30 +0300

    Photo gallery access and permission codes

--- app/src/main/java/com/example/campushere/CreateFragment.kt
+++ app/src/main/java/com/example/campushere/CreateFragment.kt
@@ -1,12 +1,27 @@
 package com.example.campushere
 
+import android.Manifest
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.campushere.databinding.FragmentCreateBinding
 import com.example.campushere.databinding.FragmentUserBinding
+import com.google.android.material.snackbar.Snackbar
 
 
 class CreateFragment : Fragment() {
@@ -14,9 +29,17 @@ class CreateFragment : Fragment() {
     private var _binding : FragmentCreateBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher : ActivityResultLauncher<String> //url string olacağı için tipi string
+    var selectedImg : Uri? =null
+    var selectedBitmap : Bitmap? =null
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        registerLauncher()
     }
 
     override fun onCreateView(
@@ -30,8 +53,100 @@ class CreateFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.imageView3.setOnClickListener { selectPicture(it) }
     }
 
+    fun selectPicture(view: View){
+
+        if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.TIRAMISU){
+            //read media images
+            if(ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED){
+                //no permission
+
+                if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.READ_MEDIA_IMAGES)){
+                    //izin mantığını kullanıcıya sunacağız
+                    Snackbar.make(view,"You have to give permission for gallery usage",Snackbar.LENGTH_INDEFINITE).setAction("Give permission",
+                        View.OnClickListener{
+                            //izin isteyeceğiz zorunlu
+                            permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                        }).show()
+                }else{
+                    //izin istememiz lazım
+                    permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                }
+            }else{
+                //write the go gallery codes
+                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            }
+        }else{
+            //read external storage (sdk e göre kullanılacak permission)
+            if(ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                //no permission
+
+                if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)){
+                    //izin mantığını kullanıcıya sunacağız
+                    Snackbar.make(view,"You have to give permission for gallery usage",Snackbar.LENGTH_INDEFINITE).setAction("Give permission",
+                        View.OnClickListener{
+                            //izin isteyeceğiz zorunlu
+                            permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        }).show()
+                }else{
+                    //izin istememiz lazım
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            }else{
+                //write the go gallery codes
+                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            }
+        }
+    }
+
+    private fun registerLauncher(){
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+
+            if (result.resultCode == RESULT_OK) {
+                val intentFromResult = result.data
+                if (intentFromResult != null) {
+                    selectedImg = intentFromResult.data
+                    try {
+                        if (Build.VERSION.SDK_INT >= 28) {
+                            val source = ImageDecoder.createSource(
+                                requireActivity().contentResolver,
+                                selectedImg!!
+                            )
+                            selectedBitmap = ImageDecoder.decodeBitmap(source)
+                            binding.imageView3.setImageBitmap(selectedBitmap)
+
+                        } else {
+                            selectedBitmap = MediaStore.Images.Media.getBitmap(
+                                requireActivity().contentResolver,
+                                selectedImg
+                            )
+                            binding.imageView3.setImageBitmap(selectedBitmap)
+                        }
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ result->
+
+            if(result){
+                //izin verildi
+                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            }else{
+                //kullanıcı izni reddedti
+                Toast.makeText(requireContext(),"No permission given. You have to give the permission.",Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
